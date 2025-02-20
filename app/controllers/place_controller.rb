608e48303e4b07d97a0c0a4127884736b6f850dc
Jacob Martinez <jacob.martinez@kellogg.northwestern.edu>
2025-02-20 17:17:02 +0000

    saving

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -1,26 +1,25 @@
 class PlaceController < ApplicationController
-def index
 
-  # find all Places rows
-  @places = Place.all 
-  # render places/index view
+  def index
+    # find all Places rows
+    @places = Place.all 
+    # render places/index view
+  end
 
-end
+  def new
+    # render a view with new places form
+  end
 
-def new
-  # render a view with new places form
-end
-
-def create
-  #   # start with a new Company
-  @places = Place.new
-  #   # assign user-entered form data to Company's columns
-  @places["name"] = params["name"]
+  def create
+    #   # start with a new Company
+    @places = Place.new
+    #   # assign user-entered form data to Company's columns
+    @places["name"] = params["name"]
   
-  #   # save Company row
-  @places.save
-  #   # redirect user
-  redirect_to "/place"
+    #   # save Company row
+    @places.save
+    #   # redirect user
+    redirect_to "/place"
   end
 
   def show
