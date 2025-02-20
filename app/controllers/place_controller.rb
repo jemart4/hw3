245ab69b129e72dc18cb6f0836518b3e9e78a08b
Jacob Martinez <jacob.martinez@kellogg.northwestern.edu>
2025-02-20 15:56:43 +0000

    can create new place

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -11,4 +11,16 @@ def new
   # render a view with new places form
 end
 
+def create
+  #   # start with a new Company
+  @places = Place.new
+  #   # assign user-entered form data to Company's columns
+  @places["name"] = params["name"]
+  
+  #   # save Company row
+  @places.save
+  #   # redirect user
+  redirect_to "/place"
+  end
+
 end
